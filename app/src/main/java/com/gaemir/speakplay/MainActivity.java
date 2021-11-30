a8f2c32a039f41bc3873c34e3d57878f846c11ac
gabriel <gaborofru@gmail.com>
2021-11-30 13:22:40 +0100

    pantalla de inicio

--- app/src/main/java/com/gaemir/speakplay/MainActivity.java
+++ app/src/main/java/com/gaemir/speakplay/MainActivity.java
@@ -3,12 +3,61 @@ package com.gaemir.speakplay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Animation animaBird, animaCorazon, animaSuperman, animaCofre, animaIronman;
+    ImageView bird, corazon, superman, cofre, ironman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bird = (ImageView) findViewById(R.id.bird);
+        corazon = (ImageView) findViewById(R.id.corazon);
+        superman = (ImageView) findViewById(R.id.superman);
+        cofre = (ImageView) findViewById(R.id.cofre);
+        ironman = (ImageView) findViewById(R.id.ironman);
+        ironman.setVisibility(View.INVISIBLE);
+
+
+        animaBird = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bird);
+        animaSuperman = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.superman);
+        animaCorazon = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.corazon);
+        animaCofre = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.cofre);
+        animaIronman = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ironman);
+
+        bird.startAnimation(animaBird);
+        superman.startAnimation(animaSuperman);
+        corazon.startAnimation(animaCorazon);
+        cofre.startAnimation(animaCofre);
+
+
+        this.cofre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view == findViewById(R.id.cofre)) {
+                    cofre.clearAnimation();
+                    cofre.setImageResource(R.drawable.cofreoabierto);
+                    int dimensionInPixel = 75;
+                    int dimensionInDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dimensionInPixel, getResources().getDisplayMetrics());
+                    cofre.getLayoutParams().height = dimensionInDp;
+                    cofre.getLayoutParams().width = dimensionInDp;
+
+                    ironman.setVisibility(View.VISIBLE);
+                    ironman.startAnimation(animaIronman);
+
+
+                }
+            }
+        });
     }
 }
