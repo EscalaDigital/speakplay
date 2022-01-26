1a28261bcb805fa290317d1bf2dc5706bc2dd4b2
gabriel <gaemir2006@hotmail.com>
2022-01-26 19:21:45 +0100

    Funcion estadísitica completa

--- app/src/main/java/com/gaemir/speakplay/estadisticasActivity.java
+++ app/src/main/java/com/gaemir/speakplay/estadisticasActivity.java
@@ -1,8 +1,6 @@
 package com.gaemir.speakplay;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -11,7 +9,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -26,14 +23,21 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
 import lecho.lib.hellocharts.model.PieChartData;
 import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
 import lecho.lib.hellocharts.view.PieChartView;
 
 public class estadisticasActivity extends AppCompatActivity {
 
-    PieChartView pieChartView;
-
+    PieChartView graficoSexo, graficoJuegos;
+    ColumnChartView graficoEdad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,6 @@ public class estadisticasActivity extends AppCompatActivity {
 
         if (networkInfo != null && networkInfo.isConnected()) {
 
-            PieChartView pieChartView = findViewById(R.id.chart);
 
             try {
                 obtenerTodos(this, Peticion.GET_ESTADISTICAS);
@@ -115,15 +118,24 @@ public class estadisticasActivity extends AppCompatActivity {
             switch (estado) {
                 case "1": // EXITO
 
-                    JSONArray mensaje = response.getJSONArray("info");
+                    JSONArray mensajeSexo = response.getJSONArray("sexo");
+                    JSONArray mensajeEdad = response.getJSONArray("edad");
+                    JSONArray mensajeJuegos = response.getJSONArray("juegos");
 
 
                     List pieData = new ArrayList<>();
-                    pieChartView = findViewById(R.id.chart);
+                    List pieDataJuegos = new ArrayList<>();
+                    List<Column> pieDataEdad = new ArrayList<>();
+                    List<SubcolumnValue> subcolumnValueList;
 
-                    for (int i = 0; i < mensaje.length(); i++) {
+
+                    graficoSexo = findViewById(R.id.chart);
+                    graficoJuegos = findViewById(R.id.chartJuegos);
+                    graficoEdad = (ColumnChartView) findViewById(R.id.chartedad);
+
+                    for (int i = 0; i < mensajeSexo.length(); i++) {
                         try {
-                            JSONObject jsonObject = mensaje.getJSONObject(i);
+                            JSONObject jsonObject = mensajeSexo.getJSONObject(i);
 
 
                             int sexo = Integer.valueOf(jsonObject.getString("sexo"));
@@ -133,15 +145,13 @@ public class estadisticasActivity extends AppCompatActivity {
                             if (sexo == 0) {
 
                                 pieData.add(new SliceValue(total, Color.BLUE).setLabel("Hombres"));
-                                /**    pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
-                                 pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
-                                 pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));*/
+
 
 
                             } else if (sexo == 1) {
-                                pieData.add(new SliceValue(total, Color.RED).setLabel("Mujeres"));
+                                pieData.add(new SliceValue(total, Color.BLUE).setLabel("Mujeres"));
                             } else if (sexo == 2) {
-                                pieData.add(new SliceValue(total, Color.MAGENTA).setLabel("Sexo oculto"));
+                                pieData.add(new SliceValue(total, Color.BLUE).setLabel("Sexo oculto"));
                             }
 
 
@@ -152,7 +162,99 @@ public class estadisticasActivity extends AppCompatActivity {
                         PieChartData pieChartData = new PieChartData(pieData);
                         pieChartData.setHasLabels(true).setValueLabelTextSize(14);
                         pieChartData.setHasCenterCircle(true).setCenterText1("Usuarios por sexo").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
-                        pieChartView.setPieChartData(pieChartData);
+                        graficoSexo.setPieChartData(pieChartData);
+                    }
+
+                    for (int i = 0; i < mensajeEdad.length(); i++) {
+                        try {
+                            JSONObject jsonObject = mensajeEdad.getJSONObject(i);
+
+
+                            int meno10 = Integer.valueOf(jsonObject.getString("menor10"));
+                            int entre1020 = Integer.valueOf(jsonObject.getString("entre1020"));
+                            int entre2030 = Integer.valueOf(jsonObject.getString("entre2030"));
+                            int entre3040 = Integer.valueOf(jsonObject.getString("entre3040"));
+                            int entre4050 = Integer.valueOf(jsonObject.getString("entre4050"));
+                            int entre5060 = Integer.valueOf(jsonObject.getString("entre5060"));
+                            int entre6070 = Integer.valueOf(jsonObject.getString("entre6070"));
+                            int entre7080 = Integer.valueOf(jsonObject.getString("entre7080"));
+                            int entre8090 = Integer.valueOf(jsonObject.getString("entre8090"));
+                            int entre90100 = Integer.valueOf(jsonObject.getString("entre90100"));
+
+                            subcolumnValueList = new ArrayList<>();
+
+                            subcolumnValueList.add(new SubcolumnValue(meno10, Color.BLUE).setLabel("Menor 10"));
+                            subcolumnValueList.add(new SubcolumnValue(entre1020, Color.BLUE).setLabel("10 < 20"));
+                            subcolumnValueList.add(new SubcolumnValue(entre2030, Color.BLUE).setLabel("20 < 30"));
+                            subcolumnValueList.add(new SubcolumnValue(entre3040, Color.BLUE).setLabel("30 < 40"));
+                            subcolumnValueList.add(new SubcolumnValue(entre4050, Color.BLUE).setLabel("40 < 50"));
+                            subcolumnValueList.add(new SubcolumnValue(entre5060, Color.BLUE).setLabel("50 < 60"));
+                            subcolumnValueList.add(new SubcolumnValue(entre6070, Color.BLUE).setLabel("60 < 70"));
+                            subcolumnValueList.add(new SubcolumnValue(entre7080, Color.BLUE).setLabel("70 < 80"));
+                            subcolumnValueList.add(new SubcolumnValue(entre8090, Color.BLUE).setLabel("80 < 90"));
+                            subcolumnValueList.add(new SubcolumnValue(entre90100, Color.BLUE).setLabel("90 < 100"));
+
+
+                            Column column = new Column(subcolumnValueList);
+                            column.setHasLabels(true);
+                            pieDataEdad.add(column);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+
+                    Axis axisX = new Axis();
+                    Axis axisY = new Axis();
+                    ArrayList<AxisValue> axisValuesY = new ArrayList<AxisValue>();
+                    //Set vertical axis name
+                    List<AxisValue> axisValuesX = new ArrayList<>();
+
+                    axisValuesX.add(new AxisValue(0).setLabel(""));
+                    axisValuesY.add(new AxisValue(0).setLabel(""));
+
+                    axisX.setValues(axisValuesX);
+
+                    axisX.setName("Tramos de edad");    //Set horizontal axis name
+                    axisY.setName("Personas");
+                    axisX.setHasLines(true);
+                    axisX.setTextSize(10);
+                    axisY.setLineColor(Color.BLACK);
+                    axisX.setTextColor(Color.BLACK);
+                    axisY.setTextColor(Color.BLACK);
+                    axisY.setLineColor(Color.BLACK);
+                    axisY.setTextSize(10);
+
+                    ColumnChartData graficoedad = new ColumnChartData(pieDataEdad);
+                    graficoedad.setAxisXBottom(axisX); //Setting horizontal axis
+                    graficoedad.setAxisYLeft(axisY);   //Setting up vertical shaft
+
+                    graficoEdad.setColumnChartData(graficoedad);
+
+                    for (int i = 0; i < mensajeJuegos.length(); i++) {
+                        try {
+                            JSONObject jsonObject = mensajeJuegos.getJSONObject(i);
+
+                            String juego = jsonObject.getString("juego");
+                            int totalusuarios = Integer.valueOf(jsonObject.getString("usuarios"));
+
+
+                                pieDataJuegos.add(new SliceValue(totalusuarios, Color.BLUE).setLabel(juego));
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        PieChartData pieChartDataJuegos = new PieChartData(pieDataJuegos);
+                        pieChartDataJuegos.setHasLabels(true).setValueLabelTextSize(14);
+                        pieChartDataJuegos.setHasCenterCircle(true).setCenterText1("Nº de usuarios").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
+                        graficoJuegos.setPieChartData(pieChartDataJuegos);
                     }
                     break;
                 case "2": // FALLIDO
